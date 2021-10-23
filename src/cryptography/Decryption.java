02fb51156e33be803cb6114ce2819350a8ea9376
Hridyanshu <hridyanshusharma@gmail.com>
2021-10-23 22:45:44 +0530

    Main java file for Decryption

---
+++ src/cryptography/Decryption.java
@@ -0,0 +1,57 @@
+package cryptography;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Decryption {
+    public static void decrypt(String fileName, String key) throws IOException {
+    	FileReader fr = new FileReader("C:\\Users\\Hridyanshu\\eclipse-workspace\\HybridCryptography\\WebContent\\uploads\\" + fileName);
+        String newFileName = "decrypted-" + fileName;
+    	FileWriter fw = new FileWriter("C:\\Users\\Hridyanshu\\eclipse-workspace\\HybridCryptography\\WebContent\\uploads\\" + newFileName);
+        int i;
+        String temp = "", temp2;
+        int counter = 1;
+        while ((i=fr.read()) != -1) {
+            if((char) i == ' ' || (char)i == '\n') {
+                if(counter==1) {
+                    temp2 = AES.decryption(temp, key);
+                    fw.write(temp2);
+                    fw.write((char)i);
+                    counter++;
+                }
+                else if(counter==2) {
+                    temp2 = RC4.decrypt(temp, key);
+                    fw.write(temp2);
+                    fw.write((char)i);
+                    counter++;
+                }
+                else if(counter==3) {
+                    temp2 = DES.decryption(temp, key);
+                    fw.write(temp2);
+                    fw.write((char)i);
+                    counter=1;
+                }
+                temp = "";
+                continue;
+            }
+            temp = temp + (char)i;
+        }
+        if(temp!=""){
+            if(counter==1) {
+                temp2 = AES.decryption(temp, key);
+                fw.write(temp2);
+            }
+            else if(counter==2) {
+                temp2 = RC4.decrypt(temp, key);
+                fw.write(temp2);
+            }
+            else {
+                temp2 = DES.decryption(temp, key);
+                fw.write(temp2);
+            }
+        }
+        fr.close();
+        fw.close();
+    }
+}
